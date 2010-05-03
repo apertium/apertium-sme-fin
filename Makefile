e4dbdd40a63b994fb73624950ca28d7195287083
Francis M. Tyers <ftyers@prompsit.com>
2010-05-03 13:08:22 +0000

    move back and update script

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 all:
 	apertium-validate-dictionary apertium-sme-fin.sme-fin.dix
 	lt-comp rl apertium-sme-fin.sme-fin.dix fin-sme.autobil.bin
+
+	apertium-validate-transfer apertium-sme-fin.fin-sme.t1x
+	apertium-preprocess-transfer apertium-sme-fin.fin-sme.t1x fin-sme.t1x.bin
--- script.sh
+++ script.sh
@@ -4,4 +4,5 @@ FINCG=/home/fran/source/giellatekno/trunk/kt/fin/src/fin-dis.bin
 sed 's/\W/\n&\n/g' | grep -v '^ $' |\
 hfst-lookup -f apertium $OMORFI/src/mor-omorfi.apertium.hfst |\
 cg-proc $FINCG |\
-apertium-tagger -g fin-sme.prob
+apertium-tagger -g fin-sme.prob |\
+apertium-transfer apertium-sme-fin.fin-sme.t1x fin-sme.t1x.bin fin-sme.autobil.bin
