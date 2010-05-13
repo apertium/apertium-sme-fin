7ab31aa8f5f4da00beb069b838adc9d40b376dec
Francis M. Tyers <ftyers@prompsit.com>
2010-05-13 14:27:16 +0000

    more

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 OMORFI=$GTHOME/kt/fin/omorfi
 FINCG=$GTHOME/kt/fin/src/fin-dis.bin
 
-sed 's/\W/\n&\n/g' | grep -v '^ $' |\
-hfst-lookup -f apertium $OMORFI/src/mor-omorfi.apertium.hfst |\
+hfst-proc $OMORFI/src/mor-omorfi.apertium.hfst |\
 cg-proc $FINCG |\
 apertium-tagger -g fin-sme.prob |\
 apertium-transfer apertium-sme-fin.fin-sme.t1x fin-sme.t1x.bin fin-sme.autobil.bin
