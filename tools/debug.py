5afab1b7f50913013c974dd715a5be9a340119d8
Ryan Johnson <ryan.txanson@gmail.com>
2010-05-31 02:07:19 +0000

    debug script can watch directory for file changes, and then reruns a command. should be compatible across OSes

--- tools/debug.py
+++ tools/debug.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python2.6
+# encoding: utf-8
+
 import os, sys, getopt
 import subprocess as sp
 P = sp.PIPE
 
 GTHOME = "/Users/pyry/gtsvn"
-OMORFI = "%s/kt/fin/omorfi" % GTHOME
+FIN = GTHOME + "/kt/fin/"
+OMORFI = "%s/kt/fin/omorfi/src" % GTHOME
+# OMORFI = "/opt/local/share/omorfi"
 FINCG = "%s/kt/fin/src/fin-dis.bin" % GTHOME
 
 # TODO: automake in apertium and gtsvn when files differ?
 
 steps = [a.split(' ') for a in 
 	[
-		"hfst-proc %s/src/mor-omorfi.apertium.hfst" % OMORFI,
-		"cg-proc -w %s" % FINCG,
+		"hfst-proc %s/mor-omorfi.apertium.hfst" % OMORFI,
+		# "cg-proc -w %s" % FINCG,
+		"preprocess | hfst-lookup /opt/local/share/omorfi/mor-omorfi.giellatekno.hfst | /Users/pyry/apertium/apertium-sme-fin/tools/lookup2cg | vislcg3 -g /Users/pyry/apertium/apertium-sme-fin/src/fin-dis.rle",
+		"./switch-label.py"
 		"apertium-tagger -g fin-sme.prob",
 		"apertium-transfer apertium-sme-fin.fin-sme.t1x fin-sme.t1x.bin fin-sme.autobil.bin",
 		# "apertium -d . fin-sme-chunker",
@@ -20,7 +26,9 @@ steps = [a.split(' ') for a in
 	]
 ]
 
-def split_output(text=sys.stdin.read()):
+def split_output(text=False):
+	if not text:
+		text = sys.stdin.read()
 	
 	outputs = []
 	input = text.replace('\n','')
@@ -33,4 +41,102 @@ def split_output(text=sys.stdin.read()):
 			
 	print '\n'.join(outputs)
 
-split_output()
+FINCGT = "preprocess | hfst-lookup /opt/local/share/omorfi/mor-omorfi.giellatekno.hfst | /Users/pyry/apertium/apertium-sme-fin/tools/lookup2cg | vislcg3 -g /Users/pyry/apertium/apertium-sme-fin/src/fin-dis.rle --trace"
+
+
+def watch(dir_to_watch, command):
+	"""
+		Watches a directory for file additions updates or saves, then runs a command; clearing screen.
+	"""
+	import os, time
+	os.system("clear")
+	os.system(command)
+	path_to_watch = dir_to_watch
+	before = dict ([(f, [None, os.stat(f).st_mtime]) for f in os.listdir (path_to_watch)])
+	
+	while 1:
+		time.sleep(1)
+		after = dict ([(f, [None, os.stat(f).st_mtime]) for f in os.listdir (path_to_watch)])
+		added = [f for f in after if not f in before]
+		removed = [f for f in before if not f in after]
+		
+		if added or removed:
+			updated = None
+		else:
+			updated = [f for f, k in after.items() if before[f][1] != after[f][1]]
+		
+		if added: print "Added: ", ", ".join (added)
+		if removed: print "Removed: ", ", ".join (removed)
+		if updated: print "Updated: ", ", ".join(updated)
+		
+		if added or updated or removed:
+			os.system("clear")
+			os.system(command)
+		before = after
+
+import getopt
+
+
+help_message = '''
+-c/--clean	Make clean in apertium-sme-fin and GTHOME/kt/fin/src
+-r/--remake	Make all in apertium-sme-fin and GTHOME/kt/fin/src
+-i/--in		Read from stdin after --clean and --remake
+-e/--exec	Run this command when --watch'ing
+-w/--watch	Watch a directory
+With no options, this script expects piped input. Otherwise, options are processed first.'''
+
+
+class Usage(Exception):
+	def __init__(self, msg):
+		self.msg = msg
+
+
+def main(argv=None):
+	if argv is None:
+		argv = sys.argv
+	try:
+		try:
+			opts, args = getopt.getopt(argv[1:], "hrciew:v", ["help", "remake", "clean", "in", "exec=", "watch="])
+		except getopt.error, msg:
+			raise Usage(msg)
+
+		# option processing
+		for option, value in opts:
+			if option == "-v":
+				verbose = True
+			if option in ("-h", "--help"):
+				print help_message
+			if option in ("-c", "--clean"):
+				sp.Popen(["make", "clean"])
+				os.system("cd %ssrc/ && make -f Makefile clean" % FIN)
+			if option in ("-r", "--remake"):
+				sp.Popen(["make"])
+				os.system("cd %ssrc/ && make -f Makefile" % FIN)
+			if option in ("-i", "--in"):
+				split_output()
+			if option in ("-e", "--exec"):
+				command = value
+				if command.startswith("\"") and command.endswith("\""):
+					command = command[1:len(command)-1]
+				if command == "fincgt":
+					command = FINCGT
+					imput = sys.stdin.read()
+					command = "echo \"%s\" | %s" % ((imput, command))
+			if option in ("-w", "--watch"):
+				dir_to_watch=value
+				watch(dir_to_watch, command)
+		
+		if len(opts) == 0:
+			split_output()
+
+	except Usage, err:
+		print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+		print >> sys.stderr, "\t for help use --help"
+		return 2
+
+
+if __name__ == "__main__":
+	sys.exit(main())
+
+
+# split_output()
