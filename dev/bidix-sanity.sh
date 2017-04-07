03e6d24ee4d336853a58b9680eb668a0a9e9883b
Trond Trosterud <trond.trosterud@uit.no>
2017-04-07 09:07:06 +0000

    Replaced old bidix-sanity (that did not work) with a copy of the sme-smn one, that had newer variables.

--- dev/bidix-sanity.sh
+++ dev/bidix-sanity.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
 
-alias gecho=echo
-alias gsed=sed
-
 pair=`pwd | grep -o 'apertium-[a-z][a-z][a-z]-[a-z][a-z][a-z]'`;
-dir=`gecho $pair | grep -o  -- '-[a-z][a-z][a-z]-[a-z][a-z][a-z]' | gsed 's/^-//g'`; 
+dir=`printf "%s\n" "$pair" | grep -o  -- '-[a-z][a-z][a-z]-[a-z][a-z][a-z]' | gsed 's/^-//g'`; 
 dix=$pair.$dir.dix
-lang2=`gecho $dir | cut -f2 -d'-'`
-gtdir=`gecho $GIELLA_CORE | gsed 's/\(giella-core\|gtcore\|core\)//g'`;
+lang2=`printf "%s\n"  "$dir" | cut -f2 -d'-'`
+gtdir=`printf "%s\n"  "$GTCORE" | gsed 's/\(gtcore\|core\|giella\-core\)//g'`;
 analysator=$gtdir"/langs/"$lang2"/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol"
-count=0
-total=0
-
-echo $pair" "$dir" "$dix" "$lang2" "$gtdir" "$analysator
 
 lt-expand ../$dix | gsed 's/\(<[^>]\+>\)\(<[^>]\+>\)\+/\1/g' | gsed 's/:[><]:/:/g'  | grep -v ':\([[:punct:]]\|[[:space:]]\)' | grep -v -- '-<' | grep -v '\/' | sort -u > /tmp/$dir.exp
 
 cat /tmp/$dir.exp | cut -f2- -d':' | cut -f1 -d'<' | hfst-proc -w $analysator > /tmp/$dir.a
 
 wc -l /tmp/$dir.exp /tmp/$dir.a
-total=`cat /tmp/$dir.exp | wc -l`;
 
 for line in `paste /tmp/$dir.exp /tmp/$dir.a  | gsed 's/\t/:/g' | gsed 's/ /¬/g'`; do
-	lang1s=`gecho $line | cut -f1 -d':'`;
-	lang2s=`gecho $line | cut -f2 -d':'`;
-	lang2m=`gecho $line | cut -f3 -d':'`;
+	lang1s=`printf "%s\n"  "$line" | cut -f1 -d':'`;
+	lang2s=`printf "%s\n"  "$line" | cut -f2 -d':'`;
+	lang2m=`printf "%s\n"  "$line" | cut -f3 -d':'`;
 
-	num=`gecho "$lang2m" | grep -o "$lang2s" | wc -l`
+	num=`printf "%s\n"   "$lang2m" | grep -o "$lang2s" | wc -l`
 #	gecho $num" "$lang2m" "$lang2s;
 	if [[ $num -eq 0 ]]; then
-		gecho -e "%\t"$line;
-		count=`expr $count + 1`;
+		printf "%%\t%s\n" "$line";
 	else
-#		gecho -e "+\t"$line;
+#		printf "%%\t%s\n" "$line";
 		continue
 	fi
 done
 	
-echo $count"/"$total
